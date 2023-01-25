e85f10be16b2d64b444babf0a0d7beb87352ea09
zubair-appiskey <zubair.moosani@appiskey.com>
2023-01-25 21:27:16 +0500

    create countdown-timer using inquirer

---
+++ index.tsx
@@ -0,0 +1,24 @@
+import inquirer from "inquirer";
+
+const countdown = async (count: number): Promise<void> => {
+  if (count > 0) {
+    console.log(`Time remaining: ${count} seconds`);
+    setTimeout(() => {
+      countdown(count - 1);
+    }, 1000);
+  } else {
+    console.log("Time's up!");
+  }
+};
+
+console.log("Welcome to the countdown timer!");
+
+const { timer } = await inquirer.prompt([
+  {
+    type: "number",
+    name: "timer",
+    message: "Enter the countdown time in seconds",
+  },
+]);
+
+countdown(timer);
