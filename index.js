a3fffae392c4dfc035426fc3f78bcce01c3cc97a
zubair-appiskey <zubair.moosani@appiskey.com>
2023-02-05 18:36:13 +0500

    npx integration

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import inquirer from "inquirer";
 const countdown = async (count) => {
     if (count > 0) {
--- index.tsx
+++ index.tsx
@@ -1,3 +1,5 @@
+#!/usr/bin/env node
+
 import inquirer from "inquirer";
 
 const countdown = async (count: number): Promise<void> => {
